CHG: Zones type change fix (convert uint8 to long int for cross entropy loss calculation), changing early stopping from 30 to 20 epochs

data_processing/glacier_zones_data.py
@@ -8,7 +8,7 @@ import numpy as np
 class ToTensorZones(object):
     def __call__(self, image, target):
         image = F.to_tensor(image)
-        target = torch.as_tensor(np.array(target))
+        target = torch.as_tensor(np.array(target)).to(torch.long)
         # value for NA area=0, stone=64, glacier=127, ocean with ice melange=254
         target[target == 0] = 0
         target[target == 64] = 1

models/networks/ParentDeepLab.py
@@ -410,7 +410,7 @@ class DeepLabv3_plus(pl.LightningModule):
                 m.bias.data.zero_()
 
     def adapt_mask(self, y):
-        mask_type = torch.float32 if self.n_classes == 1 else torch.uint8
+        mask_type = torch.float32 if self.n_classes == 1 else torch.long
         y = y.type(mask_type)
         return y
 